4335cba6e0c92a7baecc204176abf2457e704949
Manisi92 <113881581+Manisi92@users.noreply.github.com>
2023-01-29 17:45:20 +0100

    initial commit

---
+++ CozaLozaWoza.java
@@ -0,0 +1,35 @@
+
+public class CozaLozaWoza {
+	
+	public static void main(String[] args) {
+		final int LOWERBOUND = 1, UPPERBOUND = 110;
+		boolean printed;
+		for(int number = LOWERBOUND; number <= UPPERBOUND; ++number) {
+			printed = false;
+			if(number % 3 == 0) {
+				System.out.println("Coza");
+				printed = true;
+			}
+			
+			if(number % 5 == 0) {
+				System.out.println("Loza");
+				printed = true;
+			}
+			
+			if(number % 7 == 0) {
+				System.out.println("Woza");
+				printed = true;
+			}
+			
+			if(!printed) {
+				System.out.println(number);
+			}
+			
+			if(number % 11 == 0) {
+				System.out.println();
+			} else {
+				System.out.println(" ");
+			}
+		}
+	}
+}
